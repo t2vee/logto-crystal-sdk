8d29c6bd2dc43b3b42a013e57f4db2ad04404269
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-16 10:33:49 +1000

    initd sample lucky application

--- logto/client/index_constants.cr
+++
@@ -1,8 +0,0 @@
-class LogtoClient
-  STORAGE_KEY = {
-  id_token: "id_token",
-  refresh_token: "refresh_token",
-  access_token_map: "access_token_map",
-  sign_in_session: "sign_in_session",
-  }
-end
